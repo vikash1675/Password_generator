a48ad112be21b6e346d2f9b6d4fef75d619833fa
git config --global user.name <vikashkushwaha3017205@gmail.com>
2023-11-04 08:45:29 +0530

    password_generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
+  const [length, setlength] = useState(8);
+  const [numberAllow, setnumberAllow] = useState(false);
+  const [charAllow, setcharAllow] = useState(false);
+  const [password, setpassword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrstuvwxyz";
+    if (numberAllow) str += "013456789";
+    if (charAllow) str += "!@#$%^&*(){}~'";
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      console.log("dsdsd",char);
+      pass += str.charAt(char);
+    }
+    setpassword(pass);
+  }, [length, numberAllow, charAllow, setpassword]);
+
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,5)
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, setcharAllow, setnumberAllow, passwordGenerator]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full max-w-md mx-auto shadow rounded-lg px-4 my-8 text-orange-500 bg-gray-800">
+      <h1 className="text-white text-center"> Password Generator</h1>
+      <div className="flex shodow rounded-lg overflow-hidden mb-4'">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          onClick={copyPassword}
+          className="outline-none bg-blue-70 text-white px-3 py-0.5 shrink-0"
         >
-          Learn React
-        </a>
-      </header>
+          Copy
+        </button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={50}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setlength(e.target.value);
+            }}
+          />
+          <label>length:{length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllow}
+            id="numberInput"
+            onChange={(e) => {
+              setnumberAllow((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Number</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllow}
+            id="characterInput"
+            onChange={(e) => {
+              setcharAllow((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Character</label>
+        </div>
+      </div>
     </div>
   );
 }
